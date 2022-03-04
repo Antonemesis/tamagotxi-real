056ac25faf75a9190c8169f94d1eac3788588d84
Antonemesis <99951607+Antonemesis@users.noreply.github.com>
2022-03-04 08:48:22 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,25 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 2; index++) {
+        basic.showLeds(`
+            . # . # .
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            `)
+        basic.showLeds(`
+            . # . # .
+            . . # . .
+            . # # # .
+            . # # # .
+            . . # . .
+            `)
+        basic.showLeds(`
+            . # . # .
+            . . # . .
+            # # # # #
+            # # # # #
+            . . # . .
+            `)
+    }
+})
